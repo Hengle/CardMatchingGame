30c6fffdcba6bdb588574336822e368b5028d89d
Kurt Loeffler <kurtloeffler@gmail.com>
2016-09-04 13:01:33 -0700

    Changed scoring logic and UI so that each level has a specified amount of fail chances. If the player fails more than that count they lose, otherwise they win. The old way was score based and was confusing.

---
+++ Assets/Scripts/FailIcon.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine.UI;
+
+public class FailIcon:MonoBehaviour
+{
+	public Image inactiveImage;
+	public Image activeImage;
+
+	public void SetActive(bool state)
+	{
+		if (state)
+		{
+			inactiveImage.gameObject.SetActive(false);
+			activeImage.gameObject.SetActive(true);
+		}
+		else
+		{
+			inactiveImage.gameObject.SetActive(true);
+			activeImage.gameObject.SetActive(false);
+		}
+	}
+}
