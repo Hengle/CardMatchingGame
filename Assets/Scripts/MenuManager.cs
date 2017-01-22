1a00708aa065081b56fcce1475a133294549a41f
Kurt Loeffler <kurtloeffler@gmail.com>
2017-01-22 14:27:41 -0800

    Added ability to get to level select screen from splash screen.

---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuManager : MonoBehaviour
+{
+	public Transform levelSelectTransform;
+	public Transform splashScreenTransform;
+
+	private void Update()
+	{
+		if (Input.anyKeyDown || Input.GetMouseButtonDown(0))
+		{
+			if (splashScreenTransform.gameObject.activeSelf)
+			{
+				splashScreenTransform.gameObject.SetActive(false);
+				levelSelectTransform.gameObject.SetActive(true);
+			}
+		}
+		if (Input.GetKeyDown(KeyCode.Escape))
+		{
+			if (levelSelectTransform.gameObject.activeSelf)
+			{
+				levelSelectTransform.gameObject.SetActive(false);
+				splashScreenTransform.gameObject.SetActive(true);
+			}
+		}
+	}
+}
