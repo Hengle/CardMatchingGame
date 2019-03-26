029b92473da7985bb40b9b8656908453207e95dc
Kurt Loeffler <kurtloeffler@gmail.com>
2019-03-26 13:49:57 -0700

    the updated Unity Post Processing Stack is too intensive for mobile even when just using lookup table. Using a simpler color lookup effect.

--- Assets/Scripts/CameraManager.cs
+++ Assets/Scripts/CameraManager.cs
@@ -1,36 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Rendering.PostProcessing;
+using AmplifyColor;
 
 public class CameraManager : MonoBehaviour
 {
 	public Camera gameCamera;
 	public Camera backgroundCamera;
 
-	public PostProcessProfile defaultProfile;
-
-	//private PostProcessProfile profile;
-	PostProcessVolume postProcessVolume;
-
-	private void Awake()
-	{
-		//compositMaterial = new Material(Shader.Find("Hidden/CompositCameras"));
-
-		postProcessVolume = backgroundCamera.gameObject.GetComponent<PostProcessVolume>();
-		postProcessVolume.profile = Instantiate(defaultProfile);
-	}
+	public AmplifyColorEffect lutEffect;
 
 	private void Start()
 	{
 		if (Game.current.currentLevel.timeOfDay)
 		{
-			ColorGrading colorGrading;
-			postProcessVolume.profile.TryGetSettings(out colorGrading);
-			if (colorGrading != null)
-			{
-				colorGrading.ldrLut.value = Game.current.currentLevel.timeOfDay.lut;
-			}
+			lutEffect.LutTexture = Game.current.currentLevel.timeOfDay.lut;
 		}
 	}
 }
