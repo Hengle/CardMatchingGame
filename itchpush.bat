a7fce7b2bb6570f8707fca07c26a3870f97c5081
Kurt Loeffler <kurtloeffler@gmail.com>
2019-03-27 14:27:47 -0700

    Figured out you can add additional admin accounts to a game in itch.io so I can use my own account to push builds, so dont need the extra explicit identity in the push commands.

--- itchpush.bat
+++ itchpush.bat
@@ -1,4 +1,4 @@
 set /p Version=<Builds/version.txt
 
-butler -i %UserProfile%/.config/itch/rhizome push Builds/Windows/%Version%/ rhizomepublishing/antelopeup:windows --userversion-file Builds/version.txt
-butler -i %UserProfile%/.config/itch/rhizome push Builds/Mac/%Version%/ rhizomepublishing/antelopeup:osx --userversion-file Builds/version.txt
+butler push Builds/Windows/%Version%/ rhizomepublishing/antelopeup:windows --userversion-file Builds/version.txt
+butler push Builds/Mac/%Version%/ rhizomepublishing/antelopeup:osx --userversion-file Builds/version.txt
