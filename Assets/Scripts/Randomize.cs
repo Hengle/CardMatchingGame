a12f5a18060d5e8222a4a0370ebce9f50a2d04d5
Kurt Loeffler <kurtloeffler@gmail.com>
2019-04-03 15:08:35 -0700

    Made splash screen hyena laugh randomized with longer average time between laughs.

---
+++ Assets/Scripts/Randomize.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace StateBehaviours
+{
+	public class Randomize : StateMachineBehaviour
+	{
+		public string variableName = "";
+		public int minValue = 0;
+		public int maxValue = 1;
+
+		override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+		{
+			animator.SetInteger(variableName, Random.Range(minValue, maxValue+1));
+		}
+	}
+}
